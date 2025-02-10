23f4d9c4f046e64e30b972f864dc4f3ee4d1849e
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2025-02-09 17:01:07 -0700

    Added mountain pillar gen

--- Assets/Scripts/Mountain/Generator/MountainGenerator.cs
+++ Assets/Scripts/Mountain/Generator/MountainGenerator.cs
@@ -1,31 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mountain.Pillar;
 using UnityEngine;
 
 public class MountainGenerator : MonoBehaviour
 {
+    private const int PLANES_PER_FACE = 3;
     [SerializeField] private GameObject planePrefab;
     private const int PLANE_SIZE = 10;
-    const int LOAD_RANGE = 1;
+    private const float PILLAR_LENGTH = PLANES_PER_FACE*PLANE_SIZE * (1 + 2 * 1.41421356237f);
+    const int LOAD_RANGE = 4;
     const int GARBAGE_COLLETION_RATE = 250;
     private int counter = 0;
     private Dictionary<Vector2Int, GameObject> planeDict = new Dictionary<Vector2Int, GameObject>();
     private HashSet<Vector2Int> farAwayPlanes = new HashSet<Vector2Int>();
+    private Dictionary<Vector2Int, MountainPillar> pillarDict = new Dictionary<Vector2Int, MountainPillar>();
     public void Start()
     {
+        pillarDict[Vector2Int.zero] = MountainPillarFactory.GeneratePillar(Vector2Int.zero);
         for (int x = -LOAD_RANGE; x <= LOAD_RANGE; x++)
         {
-            for (int y = 0; y < LOAD_RANGE; y++)
+            for (int y = -LOAD_RANGE; y <= LOAD_RANGE; y++)
             {
-                GeneratePlane(new Vector2Int(x,y));
+                GeneratePlane(new Vector2Int(x, y),Vector2Int.zero);
             }
         }
     }
-
+    
     public void Refresh(Transform playerTransform)
     {
-        
+        Debug.Log(PILLAR_LENGTH);
+        Vector2Int pillarPosition = new Vector2Int((int)((playerTransform.position.x - PILLAR_LENGTH/2)/PILLAR_LENGTH), (int)((playerTransform.position.y - PILLAR_LENGTH/2)/PILLAR_LENGTH));
+        Debug.Log(pillarPosition);
         Vector2Int playerPosition = new Vector2Int((int)playerTransform.position.x / PLANE_SIZE, (int)playerTransform.position.y / PLANE_SIZE);
         for (int x = -LOAD_RANGE; x <= LOAD_RANGE; x++)
         {
@@ -33,7 +40,7 @@ public class MountainGenerator : MonoBehaviour
             {
                 if (y < 0) continue;
                 Vector2Int position = playerPosition + new Vector2Int(x, y);
-                GeneratePlane(position);
+                //GeneratePlane(position);
                 farAwayPlanes.Remove(position);
             }
         }
@@ -47,6 +54,7 @@ public class MountainGenerator : MonoBehaviour
             }
         }
     }
+    
 
     public void FixedUpdate()
     {
@@ -68,12 +76,41 @@ public class MountainGenerator : MonoBehaviour
         
     }
 
-    private void GeneratePlane(Vector2Int position)
+    private void GeneratePlane(Vector2Int planePosition, Vector2Int pillarPosition)
     {
-        if (planeDict.ContainsKey(position)) return;
+        
+        if (!pillarDict.ContainsKey(pillarPosition)) return;
         GameObject plane = GameObject.Instantiate(planePrefab,transform);
-        plane.transform.localPosition = PLANE_SIZE * new Vector3(position.x, position.y, 0);
-        plane.name = $"Wall[{position.x},{position.y}]";
-        planeDict[position] = plane;
+        
+        const float ROTATION = 45;
+        const float X_ROTATION = -90f;
+        float dif = Mathf.Sqrt(PLANE_SIZE * PLANE_SIZE / 2f);
+        
+        if (planePosition.x > PLANES_PER_FACE/2) // Rotated by 45
+        {
+            int diagOffset = planePosition.x - PLANES_PER_FACE/2;
+            plane.transform.localPosition = new Vector3(PLANE_SIZE/2f*(PLANES_PER_FACE/2),0,0) 
+                - 1/2f * new Vector3(dif,0,dif) 
+                + diagOffset * new Vector3(dif,0,dif) 
+                + PLANE_SIZE * new Vector3(PLANES_PER_FACE/2, planePosition.y, pillarPosition.y);
+            plane.transform.rotation = Quaternion.Euler(X_ROTATION, -ROTATION, 0);
+        } else if (planePosition.x < -PLANES_PER_FACE/2) // Rotated by -45
+        {
+            int diagOffset = planePosition.x + PLANES_PER_FACE/2;
+            plane.transform.localPosition =
+                new Vector3(PLANE_SIZE/2f*(PLANES_PER_FACE/2),0,0) 
+               - 1/2f * new Vector3(dif,0,dif) 
+               + diagOffset * new Vector3(-dif,0,dif) 
+               + PLANE_SIZE * new Vector3(-PLANES_PER_FACE/2, planePosition.y, pillarPosition.y);
+            plane.transform.rotation = Quaternion.Euler(X_ROTATION, ROTATION, 0);
+        }
+        else
+        {
+            plane.transform.localPosition = PLANE_SIZE * new Vector3(planePosition.x, planePosition.y, pillarPosition.y);
+            plane.transform.rotation = Quaternion.Euler(X_ROTATION, 0, 0);
+        }
+        
+        plane.name = $"Wall[{planePosition.x},{planePosition.y}]";
+        planeDict[planePosition] = plane;
     }
 }
